6213a931cd6c4a58475ab6fc5c7833e5fb91cb74
Haiker <mr.haiker@mail.ru>
2015-11-27 16:11:30 +0200

    navigation

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,11 @@
-var DemoCtrl = function() {
-    alert("hello");
-};
+portfolioApp.controller('ScrollController', ['$scope', '$location', '$anchorScroll',
+    function ($scope, $location, $anchorScroll) {
+        $scope.gotoMenu = function() {
+            $location.hash('menu');
+            $anchorScroll();
+        };
+        $scope.gotoTop = function () {
+            $location.hash('home');
+            $anchorScroll();
+        }
+    }]);
---
+++ js/modules.js
@@ -0,0 +1,1 @@
+var portfolioApp = angular.module('portfolioApp',[]);
