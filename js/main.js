6c06373a90d01c304606dd4758205e2e9fe48540
Haiker <mr.haiker@mail.ru>
2015-12-06 16:30:00 +0200

    add contacts

--- js/main.js
+++ js/main.js
@@ -1,55 +1,93 @@
 var main = function () {
+    var menu = $('#menu');
+    var menuToggle = $('.toggle');
+    var screen = $('body, html');
+    var heightHeader,
+        heightWork,
+        heightAbout;
 
 
+    function closeMobileMenu () {
+        (menu).removeClass('active-menu');
+        menuToggle.removeClass('open');
+    }
+
+//************************************** Собития **************************************
     $('.btn-down').click(function () {
-        var heightHeader = $('header').height();
-        $('body, html').animate({scrollTop: heightHeader}, 400);
+        screen.animate({scrollTop: $('header').height()}, 400);
     });
     $('#btn-home').click(function () {
-        $('body, html').animate({scrollTop: 0}, 400);
+        screen.animate({scrollTop: 0}, 400);
+        closeMobileMenu();
+    });
+    $('#my-work').click(function () {
+        screen.animate({scrollTop:$('header').height()}, 400);
+        closeMobileMenu();
+    });
+    $('#about').click(function () {
+        heightHeader = $('header').height();
+        heightWork = $('#my-works').height();
+        screen.animate({scrollTop: heightHeader+heightWork});
+        closeMobileMenu();
+    });
+    $('#contactNav').click(function () {
+        heightHeader = $('header').height();
+        heightWork = $('#my-works').height();
+        heightAbout = $('#about-me').height();
+        screen.animate({scrollTop: heightHeader+heightWork+heightAbout}, 400);
+        closeMobileMenu();
+    });
 
-    });
-    $('.menuBtn').click(function () {   // обработчик клика по Menu Bars
-        if($('#menu').hasClass('active-menu')) {
-            $('#menu').removeClass('active-menu');
+
+
+    $('.menu').click(function () {   // обработчик клика по Menu Bars
+        if((menu).hasClass('active-menu')) {
+            (menu).removeClass('active-menu');
         } else {
-            $('#menu').addClass('active-menu');
+            (menu).addClass('active-menu');
         }
-        if($(this).hasClass('open')) {
-            $(this).removeClass('open');
+        if(menuToggle.hasClass('open')) {
+            menuToggle.removeClass('open');
         } else {
-            $(this).addClass('open');
+            menuToggle.addClass('open');
         }
     });
+
+    // обработчик собития Scroll
     $(window).scroll(function () {
         var scrollTopWindow = $(this).scrollTop();
-        var heightHeader = $('header').height();
-        var heightAbout = $('#my-works').height();
-        // CSS variable
         var position = 'relative';
         var marginTop = 0;
+        var navigation = $('#navigation');
 
-        console.info(scrollTopWindow);
+        heightHeader = $('header').height();
+        heightWork = $('#my-works').height();
+        heightAbout = $('#about-me').height();
 
         if(scrollTopWindow >= heightHeader) {
             position = 'fixed';
-            marginTop = $('#navigation').height();
+            marginTop = navigation.height();
         }
-        $('#navigation').css('position',position);
+        navigation.css('position',position);
         $('body').css('margin-top', marginTop);
 
         if (scrollTopWindow >= heightHeader) {
             $('#my-work').addClass('active-link');
         } else {
             $('#my-work').removeClass('active-link');
-            $('#about').removeClass('active-link');
         }
-        console.log(heightAbout);
-        if (scrollTopWindow >= heightHeader+heightAbout) {
+        if (scrollTopWindow >= heightHeader+heightWork) {
             $('#about').addClass('active-link');
             $('#my-work').removeClass('active-link');
         } else {
             $('#about').removeClass('active-link');
+        }
+        if (scrollTopWindow >= heightHeader+heightWork+heightAbout) {
+            $('#contactNav').addClass('active-link');
+            $('#about').removeClass('active-link');
+            $('#my-work').removeClass('active-link');
+        } else {
+            $('#contactNav').removeClass('active-link');
         }
 
     });
